27b8abfc2b2ef5829f2c9313488f4e47f728ef19
Vlados <hanovvladik@mail.ru>
2026-03-12 15:19:14 +0300

    Step 7 arrays

---
+++ code/Step7_MaxElement/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = { 5, 8, 3, 1, 9 };
+
+int max = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+}
+
+Console.WriteLine($"Максимальный элемент: {max}");
