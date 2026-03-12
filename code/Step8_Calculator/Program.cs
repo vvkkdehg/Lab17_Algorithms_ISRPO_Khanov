454f1d3cbd075670e55c000b0e22e1ceae855a53
Vlados <hanovvladik@mail.ru>
2026-03-12 16:05:18 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Sum(num1, num2));
+Console.WriteLine(Sub(num1, num2));
+Console.WriteLine(Mul(num1, num2));
+Console.WriteLine(Div(num1, num2));
+
+static int Sum(int x, int y) { return x + y; }
+static int Sub(int x, int y) { return x - y; }
+static int Mul(int x, int y) { return x * y; }
+static int Div(int x, int y) { return x / y; }
